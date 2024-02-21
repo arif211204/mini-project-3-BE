4ab05c25c4c17512ba146220817af11405a48bf4
arif123 <nashrullah2344@gmail.com>
2024-02-21 13:12:08 +0700

    -

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,7 @@ module.exports = {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
+    port:process.env.MYSQL_PORT,
     host: process.env.MYSQL_HOST,
     dialect: process.env.MYSQL_DIALECT,
   },
